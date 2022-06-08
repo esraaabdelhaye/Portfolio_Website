c535441b22166c68e5da2fb7fc3755cd2f7895f8
Esraa Abdelhay <88619864+esraaabdelhaye@users.noreply.github.com>
2022-06-08 23:10:03 +0200

    Second Day Done
    The Website Is Nearly Finished

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const Header = document.querySelector('.header')
 window.addEventListener('scroll', ()=> changeNavColor(window.scrollY))
 
 function changeNavColor(scrollY){
-    const Color = scrollY >=200? '#f0f0f0' : 'transparent';
+    const Color = scrollY >=150? '#f0f0f0' : 'transparent';
     Header.style.backgroundColor= Color;
 }
