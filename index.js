719168637bb944e00482acee6cecd3b88a76e0d7
Esraa Abdelhay <88619864+esraaabdelhaye@users.noreply.github.com>
2022-06-09 11:44:40 +0200

    The Website Is Done Now

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const Header = document.querySelector('.header')
-
+const toggleBtn = document.querySelector('.toggle');
+const navBar = document.querySelector(".navigation")
+const I = document.querySelector(".fa-bars")
 window.addEventListener('scroll', ()=> changeNavColor(window.scrollY))
 
 function changeNavColor(scrollY){
     const Color = scrollY >=150? '#f0f0f0' : 'transparent';
     Header.style.backgroundColor= Color;
+}
+
+toggleBtn.addEventListener('click' , showHideNavBar)
+
+function showHideNavBar(){
+    navBar.classList.toggle("hide")
+    const btnClasss = navBar.classList.contains('hide')?'bars':'times';
+    I.classList=`fa fa-${btnClasss}`;
 }
